e2aa04e99ee960c1a80bf2e444e3b10bd8d9c710
charlierothschild <c_rothschild@coloradocollege.edu>
2025-02-27 15:05:30 -0700

    add pdf functionality

--- pdftotext.py
+++ pdftotext.py
@@ -2,24 +2,63 @@ import sys
 from PIL import Image
 from pdf2image import convert_from_path, convert_from_bytes
 import pytesseract
+from fpdf import FPDF
+import os
+import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError,
     PDFPageCountError,
     PDFSyntaxError
 )
 
+
+def img_to_txt(image):
+    return pytesseract.image_to_string(image)
+
+def add_page_to_pdf(text, output_file):
+    pdf = FPDF()
+    text2 = text.encode('latin-1', 'replace').decode('latin-1')
+    pdf.add_page()
+    pdf.multi_cell(0, 10, txt=text2)
+
+
+def pdf_to_txt(file_path, output_file):
+    images = convert_from_path(file_path)
+    text = ""
+    for pagenum in range(len(images)):
+        text += img_to_txt(images[pagenum])
+        
+        print(f"page {pagenum + 1}/{len(images)} done!")
+    
+    with open(output_file, 'w') as text_file:
+        text_file.write(text)
+    
+    return text
+
+def get_chatgpt_response(prompt):
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("invalid arguments provided")
         exit()
-    file_path = sys.argv[1]
-    print(file_path)
-    images = convert_from_path(file_path)
-    print(images)
-    text = ""
-    for image in images:
-        text += pytesseract.image_to_string(image)
+    
 
-    with open("output.txt", "w") as file:
-        file.write(text)
+    file_path = sys.argv[1]
+
+    pdf_to_txt(file_path, sys.argv[2])
+    
+
+    
+
         
